65a8f4f017bb88606e34cae7096ec5d7496c4498
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-06-03 16:16:59 +0100

    a

---
+++ category-service/src/test/java/com/pelosi/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelosi.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
